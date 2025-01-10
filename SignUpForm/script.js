311cec518925a85e10ad8fce3935fa62e2f4ffbc
John <johnprobussr@gmail.com>
2025-01-10 13:07:32 -0600

    add practice projects, dark mode and pricing cards

--- SignUpForm/script.js
+++ SignUpForm/script.js
@@ -1,7 +1,6 @@
-const container = document.querySelector(".container");
-const signUpBtn = document.querySelector(".green-bg button")
-
+const container = document.querySelector('.container');
+const signUpBtn = document.querySelector('.green-bg button');
 
 signUpBtn.addEventListener('click', () => {
-    container.classList.toggle('change');
+  container.classList.toggle('change');
 });
