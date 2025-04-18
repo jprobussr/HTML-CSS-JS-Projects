0afffaaa85aa17955054fed2ea7ac5cfff4a8873
John <johnprobussr@gmail.com>
2025-04-18 11:23:36 -0500

    add sideBar menu project

---
+++ HamburgerMenu1/app.js
@@ -0,0 +1,7 @@
+document.querySelector('.hamburger-menu').addEventListener('click', () => {
+    document.querySelector('.navigation').classList.toggle('change');
+})
+
+
+// Grab Hamburger Menu and add EventListner
+// Inside grab Navigation class - and add classList toggle change
