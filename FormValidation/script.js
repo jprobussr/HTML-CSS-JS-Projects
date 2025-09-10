42a6340ef45d9d90550dd19ca216bf8a001f2a23
John Probus Sr <105514234+jprobussr@users.noreply.github.com>
2025-09-10 16:47:52 -0500

    Update and add dark theme to project

---
+++ FormValidation/script.js
@@ -0,0 +1,9 @@
+const container = document.querySelector('.container');
+
+document.querySelector('.signup-btn').addEventListener('click', () => {
+  container.classList.add('change');
+});
+
+document.querySelector('.signin-btn').addEventListener('click', () => {
+    container.classList.remove('change')
+})
